11f6f642181a9943b0340da31d493b671a06e190
Ricky <rickdhanota@gmail.com>
2021-01-07 20:48:52 -0800

    first react proj with components

---
+++ src/SomeClassComponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './SomeClassComponent.css';
+
+export default class SomeClassComponent extends Component {
+    render() {
+        return (
+            <div className = "class_component_head">
+                This is my first class component!!
+            </div>
+        )
+    }
+}

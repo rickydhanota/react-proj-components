a6f649cf61055137be6a3d3302b4d6021fea030d
Ricky <rickdhanota@gmail.com>
2021-01-12 00:21:35 -0800

    added a button for state

--- src/PersonCard.js
+++ src/PersonCard.js
@@ -3,15 +3,22 @@ import "./PersonCard.css";
 
 //This is a component
 
-export default class PropItUp extends Component {
+export default class PersonCard extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            ageUp:this.props.age,
+        };
+    }
     render() {
         const {firstName, lastName, age, hairColor} = this.props;
         return (
             <div className = "personCard">
                 <h3 className = "Names">{lastName}, {firstName}</h3>
-                <p className = "Age">Age: {age}</p>
+                <p className = "Age">Age: {this.state.ageUp}</p>
                 <p className = "Hair-Color">Hair Color: {hairColor}</p>
-                <button onClick = {() => alert("Button clicked")}>Click Me</button>
+                <button onClick = {() => this.setState({ageUp: this.state.ageUp + 1})}>Increase Age</button>
+                {/* <button onClick = {() => alert("Button clicked")}>Click Me</button> */}
             </div>
         )
     }

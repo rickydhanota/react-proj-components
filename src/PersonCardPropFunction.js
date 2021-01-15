a673468b3bf220491645d534094b779a2783846d
Ricky <rickdhanota@gmail.com>
2021-01-14 22:54:01 -0800

    modified the prop function

--- src/PersonCardPropFunction.js
+++ src/PersonCardPropFunction.js
@@ -1,15 +1,17 @@
 import React from 'react'
 
 const PersonCardPropFunction = props => {
+    const { lastName, firstName, age, hairColor } = props;
     return (
-        <div>
-            <h1>{ props.lastName }, { props.firstName }</h1>
-            <p>Age: { props.age }</p>
-            <p>Hair Color: { props.hairColor }</p>
-        </div>
-    )
+      <div className="container">
+        <h2>
+          {lastName}, {firstName}
+        </h2>
+        <p>Age: {age}</p>
+        <p>Hair Color: {hairColor}</p>
+      </div>
+    );
 }
-
 
 
 export default PersonCardPropFunction
